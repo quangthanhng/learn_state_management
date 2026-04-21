41990f7d798dcdac88f6fc4a2126d1663af6ef40
nguyenquangthanh <quangthanh.nguyen2512@gmail.com>
2026-04-21 07:51:04 +0700

    Future Provider, stateprovider in flutter

--- example3/lib/main.dart
+++ example3/lib/main.dart
@@ -7,32 +7,6 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-extension OptionalInfixAddition<T extends num> on T? {
-  T? operator +(T? other) {
-    final shadow = this;
-    if (shadow != null) {
-      return shadow + (other ?? 0) as T;
-    } else {
-      return null;
-    }
-  }
-}
-
-class Counter extends StateNotifier<int?> {
-  Counter() : super(null);
-  void increment() => state = state == null ? 1 : state + 1;
-}
-
-final counterProvider = StateNotifierProvider<Counter, int?>(
-  (ref) => Counter(),
-);
-
-// void TestIt() {
-//   final int? int1 = 1;
-//   final int int2 = 1;
-//   final result = (int1 ?? 0) + int2;
-// }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -49,38 +23,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
+typedef WeatherEmoji = String;
+
+enum City { stockholm, paris, tokyo }
+
+Future<WeatherEmoji> getWeather(City city) {
+  return Future.delayed(
+    const Duration(seconds: 1),
+    () =>
+        {City.stockholm: '❄️', City.paris: '🌧️', City.tokyo: '🌥️'}[city] ??
+        '?',
+  );
+}
+
+// UI writes to and read from this
+final currencityProvider = StateProvider<City?>((ref) => null);
+//UI read this
+const unknownWeatherEmoji = '🤷';
+
+final weatherProvider = FutureProvider<WeatherEmoji>((ref) {
+  final city = ref.watch(currencityProvider);
+  if (city != null) {
+    return getWeather(city);
+  } else {
+    return unknownWeatherEmoji;
+  }
+});
+
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterProvider);
+    final currentWeather = ref.watch(weatherProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Consumer(
-          builder: (context, ref, child) {
-            final counter = ref.watch(counterProvider);
-            final text = counter == null
-                ? 'Press the button'
-                : counter.toString();
-            return Text(text);
-          },
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Counter display: '),
-            const Gap(400),
-            Text(counter.toString()),
-            TextButton(
-              onPressed: ref.read(counterProvider.notifier).increment,
-              child: Text('Increment counter'),
+      appBar: AppBar(title: const Text('Title weather')),
+      body: Column(
+        children: [
+          currentWeather.when(
+            data: (data) => Text(data, style: const TextStyle(fontSize: 40)),
+            error: (error, stackTrace) => const Text('Error 😢'),
+            loading: () => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const CircularProgressIndicator(),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: City.values.length,
+              itemBuilder: (context, index) {
+                final city = City.values[index];
+                final isSelected = city == ref.watch(currencityProvider);
+                return ListTile(
+                  title: Text(city.toString()),
+                  trailing: isSelected ? const Icon(Icons.check) : null,
+                  onTap: () =>
+                      ref.read(currencityProvider.notifier).state = city,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
