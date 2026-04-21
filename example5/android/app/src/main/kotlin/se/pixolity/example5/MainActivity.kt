76daf18f3780524d2a90951cc1e120a563f818dd
nguyenquangthanh <quangthanh.nguyen2512@gmail.com>
2026-04-21 11:11:02 +0700

    Complete example 5 with the unmodifiablelistview with the entire crud listview

---
+++ example5/android/app/src/main/kotlin/se/pixolity/example5/MainActivity.kt
@@ -0,0 +1,5 @@
+package se.pixolity.example5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ example5/lib/main.dart
@@ -0,0 +1,196 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:hooks_riverpod/legacy.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String uuid;
+  Person({required this.name, required this.age, String? uuid})
+    : uuid = uuid ?? const Uuid().v4();
+
+  Person updated([String? name, int? age]) =>
+      Person(name: name ?? this.name, age: age ?? this.age, uuid: uuid);
+
+  String get displayName => '$name ($age yeas old)';
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+
+  @override
+  // TODO: implement hashCode
+  int get hashCode => uuid.hashCode;
+
+  @override
+  String toString() => 'Person(name: $name, age: $age, uuid: $uuid)';
+}
+
+class DataModel extends ChangeNotifier {
+  final List<Person> _people = [];
+
+  int get count => _people.length;
+
+  UnmodifiableListView<Person> get people => UnmodifiableListView(_people);
+  // UnmodifiableListView is a kind of data structure
+  // that allows
+  void addPerson(Person person) {
+    _people.add(person);
+    notifyListeners();
+  }
+
+  void remove(Person person) {
+    _people.remove(person);
+    notifyListeners();
+  }
+
+  void update(Person updatedPerson) {
+    final index = _people.indexOf(updatedPerson);
+    final oldPerson = _people[index];
+    if (oldPerson.name != updatedPerson.name ||
+        oldPerson.age != updatedPerson.age) {
+      _people[index] = oldPerson.updated(updatedPerson.name, updatedPerson.age);
+      notifyListeners();
+    }
+  }
+}
+
+final peopleProvider = ChangeNotifierProvider((ref) => DataModel());
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: Text('HomePages')),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final dataModel = ref.watch(peopleProvider);
+          return ListView.builder(
+            itemCount: dataModel.count,
+            itemBuilder: (context, index) {
+              final person = dataModel.people[index];
+              return ListTile(
+                title: GestureDetector(
+                  onTap: () async {
+                    final updatedPerson = await createOrUpdatePersonDialog(
+                      context,
+                      person,
+                    );
+                    if (updatedPerson != null) {
+                      dataModel.update(updatedPerson);
+                    }
+                  },
+                  child: Text(person.displayName),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final person = await createOrUpdatePersonDialog(context);
+          if (person != null) {
+            final dataModel = ref.read(peopleProvider);
+            dataModel.addPerson(person);
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+final nameController = TextEditingController();
+final ageController = TextEditingController();
+
+Future<Person?> createOrUpdatePersonDialog(
+  BuildContext context, [
+  Person? existingPerson,
+]) {
+  String? name = existingPerson?.name;
+  int? age = existingPerson?.age;
+
+  nameController.text = name ?? '';
+  ageController.text = age?.toString() ?? '';
+
+  return showDialog<Person?>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Create a person'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                label: Text('Enter name here...'),
+              ),
+              onChanged: (value) => name = value,
+            ),
+
+            TextField(
+              controller: ageController,
+              decoration: const InputDecoration(
+                label: Text('Enter age here...'),
+              ),
+              onChanged: (value) => age = int.tryParse(value),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancle'),
+          ),
+          TextButton(
+            onPressed: () {
+              if (name != null && age != null) {
+                if (existingPerson != null) {
+                  // have existing person
+                  final newPerson = existingPerson.updated(name, age);
+                  Navigator.pop(context, newPerson);
+                } else {
+                  // No existing person, create a new one
+                  Navigator.pop(context, Person(name: name!, age: age!));
+                }
+              } else {
+                // no name or age, or both
+                Navigator.pop(context);
+              }
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      );
+    },
+  );
+}
